00a4f081740d1e94a77f3e57c24c042e91fd072c
redlaser42 <120990408+redlaser42@users.noreply.github.com>
2024-08-01 20:20:22 -0400

    Inital

---
+++ HideBetaPatch.cs
@@ -0,0 +1,47 @@
+﻿using HarmonyLib;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using EFT;
+using System.Runtime.CompilerServices;
+using EFT.UI;
+using UnityEngine;
+using SPT.Core.Patches;
+using SPT.Reflection.Patching;
+
+//this example patch will limit the number of jumps you can do to 3, and log whether or not your jump was successful
+
+namespace HideBetaRework.Patches
+{
+    internal class HideBetaReworkPatch : ModulePatch //we must inherit ModulePatch for our patch to work.
+    {
+        protected override MethodBase GetTargetMethod()
+        {
+            //methods are patched by targeting both their class name and the name of the method itself.
+            //the example in this patch is the Jump() method in the Player class
+            return AccessTools.Method(typeof(MenuScreen), nameof(MenuScreen.method_3));
+        }
+
+
+        [PatchPrefix]
+        static bool Prefix()
+        {
+            //code here will run BEFORE original code is executed.
+            //if 'true' is returned, the original code will still run.
+            //if 'false' is returned, the original code will be skipped.
+            return true;
+        }
+
+        [PatchPrefix]
+        static void Postfix(MenuScreen __instance, GameObject ____alphaWarningGameObject)
+        {
+            ____alphaWarningGameObject.SetActive(false);
+            //code here will run AFTER the original code is executed.
+        }
+
+        //don't forget to add 'new FiniteJumpPatch().Enable();' to the Awake() method of your Plugin.cs script to enable this patch.
+    }
+}
---
+++ Plugin.cs
@@ -0,0 +1,30 @@
+using BepInEx;
+using BepInEx.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using HideBetaRework.Patches;
+using System.Reflection;
+
+namespace HideBetaRework
+{
+    //first string below is your plugin's GUID, it MUST be unique to any other mod. Read more about it in BepInEx docs. Be sure to update it if you copy this code.
+    [BepInPlugin("redlaser42.HideBetaRework", "HideBetaRework", "0.0.1")]  
+    public class Plugin : BaseUnityPlugin
+    {
+        public static ManualLogSource LogSource;
+
+        //BaseUnityPlugin inherits MonoBehaviour, so you can use base unity functions like Awake() and Update()
+        private void Awake() //Awake() will run once when your plugin loads
+        {
+            //we save the Logger to our LogSource variable so we can use it anywhere, such as in our patches via Plugin.LogSource.LogInfo(), etc.
+            LogSource = Logger;
+            LogSource.LogInfo("plugin loaded!");
+
+            //uncomment the line below and replace "PatchClassName" with the class name you gave your patch. Patches must be enabled like this to work.
+            new HideBetaReworkPatch().Enable();            
+        }
+    }
+}
